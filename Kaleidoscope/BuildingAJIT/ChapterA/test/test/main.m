b4a3a46f121a181cac8135dbc69bb737bbb115d2
palanceli <palanceli@163.com>
2018-09-25 17:33:10 +0800

    commit changes

--- Kaleidoscope/BuildingAJIT/ChapterA/test/test/main.m
+++ Kaleidoscope/BuildingAJIT/ChapterA/test/test/main.m
@@ -8,17 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+void testEntry(){
+  float x = 0;
+  for(int i=1; i<1000000; i++){
+    if(i % 3 == 0){
+      x = x * i;
+    }else if(i % 7 == 0){
+      x = x / i;
+    }else if(i % 11 == 0){
+      x = x - i;
+    }else{
+      x = x + i;
+    }
   }
-  return 0;
+
+	// NSLog(@"x=%f\n", x);
+  NSLog(@"Hello, test entry!");
 }
 
-void testEntry(){
+int main(int argc, const char * argv[]) {
   @autoreleasepool {
-    // insert code here...
-    NSLog(@"Hello, test entry!");
+    double time_begin = CFAbsoluteTimeGetCurrent();
+    testEntry();
+    double time_end = CFAbsoluteTimeGetCurrent();
+    NSInteger time_interval = 1000 * (time_end - time_begin);  //单位毫秒	
+    NSLog(@"time_interval: %ld. \n", time_interval);
   }
+  return 0;
 }
--- Kaleidoscope/BuildingAJIT/ChapterA/toy.cpp
+++ Kaleidoscope/BuildingAJIT/ChapterA/toy.cpp
@@ -4,14 +4,24 @@
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/ExecutionEngine/GenericValue.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 using namespace llvm;
 using namespace std;
 
 int main() {
+  char* szDir;
+  if((szDir = (char*)getcwd(NULL, 0)) == NULL){
+    printf("Failed to get cwd\n");
+    return -1;
+  }
+  printf("current dir is: %s\n", szDir);
+  
   InitializeNativeTarget();
   InitializeNativeTargetAsmPrinter();
   InitializeNativeTargetAsmParser();
+  printf("--- 0\n");
   
   SMDiagnostic SMD;
   LLVMContext context;
@@ -19,12 +29,26 @@ int main() {
   const string llPath = "/Users/palance/Documents/GitHub/llvm_examples/Kaleidoscope/BuildingAJIT/ChapterA/test/test/main.ll";
   const string funcName = "testEntry";
   EngineBuilder EB(parseIRFile(llPath, SMD,context));
+  printf("--- 1\n");
   EB.setEngineKind(EngineKind::Interpreter);
+  printf("--- 2\n");
   EB.setErrorStr(&err);
+  printf("--- 3\n");
   ExecutionEngine* EE = EB.create();
+  printf("--- 4\n");
+  
   EE->runStaticConstructorsDestructors(false);
   Function* func=EE->FindFunctionNamed(funcName);
+  
+  struct timeval tvBegin, tvEnd;
+  gettimeofday(&tvBegin, NULL);
+  
   EE->runFunction(func,ArrayRef<GenericValue>());
+  
+  gettimeofday(&tvEnd, NULL);
+  //<1>.获得毫秒数:
+  double dDuration = 1000 * (tvEnd.tv_sec - tvBegin.tv_sec) + ((tvEnd.tv_usec - tvBegin.tv_usec) / 1000.0);
+  printf("dDuration=%.2f ms. \n", dDuration);
   
   return 0;
 }
