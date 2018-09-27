ce985f400bc3ba52f7ba0a6bc1dcb56a4c0155d9
palance <palanceli@163.com>
2018-09-27 09:58:38 +0800

    commit changes

--- Kaleidoscope/BuildingAJIT/ChapterA/toy.cpp
+++ Kaleidoscope/BuildingAJIT/ChapterA/toy.cpp
@@ -6,11 +6,27 @@
 #include <llvm/ExecutionEngine/GenericValue.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <dlfcn.h>
 
 using namespace llvm;
 using namespace std;
 
-int main() {
+//extern "C" {
+//char * __CFConstantStringClassReference = "";
+//}
+
+int main(int argc, char ** argv, char **envp) {
+  
+  void *handle = dlopen("/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation", RTLD_LAZY|RTLD_GLOBAL);
+//  void *sym = dlsym(handle, "__CFConstantStringClassReference");
+//  printf("__CFConstantStringClassReference: %p\n", sym);
+//  for (char **env = envp; *env != 0; env++)
+//  {
+//    char *thisEnv = *env;
+//    printf("%s\n", thisEnv);
+//  }
+  
+  
   char* szDir;
   if((szDir = (char*)getcwd(NULL, 0)) == NULL){
     printf("Failed to get cwd\n");
@@ -21,7 +37,6 @@ int main() {
   InitializeNativeTarget();
   InitializeNativeTargetAsmPrinter();
   InitializeNativeTargetAsmParser();
-  printf("--- 0\n");
   
   SMDiagnostic SMD;
   LLVMContext context;
@@ -29,13 +44,9 @@ int main() {
   const string llPath = "/Users/palance/Documents/GitHub/llvm_examples/Kaleidoscope/BuildingAJIT/ChapterA/test/test/main.ll";
   const string funcName = "testEntry";
   EngineBuilder EB(parseIRFile(llPath, SMD,context));
-  printf("--- 1\n");
   EB.setEngineKind(EngineKind::Interpreter);
-  printf("--- 2\n");
   EB.setErrorStr(&err);
-  printf("--- 3\n");
   ExecutionEngine* EE = EB.create();
-  printf("--- 4\n");
   
   EE->runStaticConstructorsDestructors(false);
   Function* func=EE->FindFunctionNamed(funcName);
