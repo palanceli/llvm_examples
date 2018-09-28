eea47a4d65baada3aab60bb62b2ac6f691bd2062
palanceli <palanceli@163.com>
2018-09-28 22:23:57 +0800

    commit changes

--- setupenv.sh
+++ setupenv.sh
@@ -1,4 +1,11 @@
-# 修改llvm-6.0.1.src/examples/Kaleidoscope/BuildingAJIT/CMakeLists.txt，添加ChapterA:
+# 修改${LLVM_DIR}/examples/Kaleidoscope/BuildingAJIT/CMakeLists.txt，添加ChapterA:
 # add_subdirectory(ChapterA)
 
-ln -s $HOME/Documents/GitHub/llvm_examples/Kaleidoscope/BuildingAJIT/ChapterA $HOME/Documents/GitHub/llvm-6.0.1.src/examples/Kaleidoscope/BuildingAJIT/ChapterA
+LLVM_DIR="$HOME/Documents/GitHub/llvm"
+LLVM_EXAMPLES_DIR="$HOME/Documents/GitHub/llvm_examples"
+
+ln -s ${LLVM_EXAMPLES_DIR}/Kaleidoscope/BuildingAJIT/ChapterA ${LLVM_DIR}/examples/Kaleidoscope/BuildingAJIT/ChapterA
+
+# # 修改${LLVM_DIR}/tools/clang/examples/CMakeLists.txt，添加FindClassDecls:
+# add_subdirectory(FindClassDecls)
+ln -s ${LLVM_EXAMPLES_DIR}/FindClassDecls ${LLVM_DIR}/tools/clang/examples/FindClassDecls
